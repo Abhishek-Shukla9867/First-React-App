33a5f6126ffbd185e296a39b274849ac19cbaa92
Abhishek Shukla <121942566+Abhishek-Shukla9867@users.noreply.github.com>
2024-04-17 22:32:30 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+     
+     <div className="w-full  h-12 flex justify-center text-center bg-black text-white  rounded mx-3 my-3 py-2">
+
+             copyright @2021 Terms and Condition
+      
+
+
+     </div>
+
+
+     
+    
+    
+    </>
+  )
+}
+
+export default Footer
